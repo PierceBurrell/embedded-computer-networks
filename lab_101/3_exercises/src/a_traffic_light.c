16d381fcb7b8762442cca78be1dee387f1d4e748
PierceBurrell <43850056+PierceBurrell@users.noreply.github.com>
2018-10-11 13:39:06 +0100

    Lights Work In Cycle
    All 3 Lights Ligh Up Now

--- lab_101/3_exercises/src/a_traffic_light.c
+++ lab_101/3_exercises/src/a_traffic_light.c
@@ -20,8 +20,10 @@
 
 // map the led to GPIO PI1 (again, this is the inbuilt led)
 gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
-gpio_pin_t led2 = {PB_14, GPIOI, GPIO_PIN_14};
-gpio_pin_t led3 = {PB_15, GPIOI, GPIO_PIN_15};
+
+gpio_pin_t led2 = {PB_14, GPIOB, GPIO_PIN_14};
+
+gpio_pin_t led3 = {PB_15, GPIOB, GPIO_PIN_15};
 
 // this is the main method
 int main()
@@ -33,29 +35,26 @@ int main()
   
   // initialise the gpio pins
   init_gpio(led, OUTPUT);
-//	init_gpio(led2, OUTPUT);
-//	init_gpio(led3, OUTPUT);
+	init_gpio(led2, OUTPUT);
+	init_gpio(led3, OUTPUT);
+	
   
   // loop forever ...
   while(1)
   {
     // toggle the led on the gpio pin
-    toggle_gpio(led);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+    toggle_gpio(led);    
+    HAL_Delay(100);
 		
-		    // toggle the led on the gpio pin
+		// toggle the led on the gpio pin
     toggle_gpio(led2);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+    HAL_Delay(100);
 		
-		    // toggle the led on the gpio pin
+		// toggle the led on the gpio pin
     toggle_gpio(led3);
-    
-    // wait for 1 second
-    HAL_Delay(1000);
+    HAL_Delay(100);
+		
+		// comment to check update
 		
 
   }
